a9e8d4201fe668ac79ce32d5ff56e3e50c0a491e
Elly-cat <ellyce123456@gmail.com>
2025-11-10 13:16:49 +0100

    funny

--- scripts/Flowcytomtry.R
+++ scripts/Flowcytomtry.R
@@ -1,5 +1,5 @@
 library(CytoPipeline)
-
+renv::activate()
 # Halen we uit de CytoPipeline package
 # output files
 workDir <- suppressMessages(base::tempdir())
@@ -42,12 +42,12 @@ sampleFile = sampleFiles(pipL_flowAI)[1])
 # handig voor begrijpen wat de pipeline heeft gedaan
 # uncomment om daar te kijken
 
-library(CytoPipelineGUI)
-if (interactive())
-  CytoPipelineCheckApp(dir = workDir) 
-
-if  (interactive())
-  ScaleTransformApp(dir = workDir)
+#library(CytoPipelineGUI)
+#if (interactive())
+#  CytoPipelineCheckApp(dir = workDir) 
+#
+#if  (interactive())
+#  ScaleTransformApp(dir = workDir)
 
 
 ### Vistualiseren van de cytometrie data ###
@@ -74,9 +74,7 @@ path = workDir
 # plot de data
 library(gridExtra)
 library(plotly)
-gg_raw <- ggplotEvents(ff_raw, yChannel = "FSC-H", xChannel = "FSC-A") + 
-  ggtitle("raw")
-gg_de_douplet <- ggplotEvents(ff_de_douplet, yChannel = "FSC-H", xChannel = "FSC-A") +
-  ggtitle("processed")
+gg_raw <- ggplotEvents(ff_raw, yChannel = "FSC-H", xChannel = "FSC-A")
+gg_de_douplet <- ggplotEvents(ff_de_douplet, yChannel = "FSC-H", xChannel = "FSC-A")
 gg_grid <- grid.arrange(gg_raw, gg_de_douplet, ncol = 2)
 ggplotly(gg_raw)
